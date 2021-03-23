fb0c6a0927791c4ea5d65bb2f67b767872b78ff9
ppidcard <tvxqlikei@live.cn>
2021-03-24 00:16:41 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+  function writeCards (names, message_type) {
+    let message_=[];
+    for (let i = 0; i < names.length; i++) {
+        message_.push(`Thank you, ${names[i]}, for the wonderful ${message_type} gift!`);
+    }
+
+       return message_;
+    }
+
+    function countDown (number_) {
+        for (let i=number_; i >=0; i--) {
+          console.log (i);
+        }
+      }
